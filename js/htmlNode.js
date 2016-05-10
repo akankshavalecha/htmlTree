88fd0d9fb4de3b4ba0f6f9f130383d0bd514ce4e
Akanksha Valecha <akanksha.valecha@gmail.com>
2016-05-10 20:03:19 +0530

    modal done; searching done; created a selector that parses tree and searches for tag, class, id

--- js/htmlNode.js
+++ js/htmlNode.js
@@ -4,7 +4,7 @@
     this.id = '';
     this.body = '';
     this.children = [];
-    this.parent = '';
+    this.parent = null;
     this.addChildren = function(n){
       this.children.push(n);
     }
@@ -52,7 +52,7 @@
    
   }
 
-  function getElementByID(id){
+  function getElementById(id){
     return idMap[id];
   }
 
@@ -64,7 +64,7 @@
     // lets say classes = 'abc'
     // on split by comma, list always will going to have length of 1, so we will check if it is less than 2
     // on split by space, list will have length of 1
-    if((tag.replace(/[ ]+/g,'').split(',')).length < 2 && tag.replace(/[ ]+/g,' ').split(' ').length > 1){
+    if(((tag.replace(/[ ]+/g,'').split(',')).length < 2) && (tag.replace(/[ ]+/g,' ').split(' ').length > 1)){
       tagList = tag.replace(/[ ]+/g,' ').split(' ');
       commaflag = false; // split is on spaces
     }else{
@@ -79,20 +79,23 @@
       for (var i = tagList.length - 1; i >= 0; i--) {
         res[tagList[i]] = tagMap[tagList[i]];
       }    
+      console.log(res)
     }
     // for space seperated tags -> parent children
     // right to left check for tags
     if(!commaflag){
-      var res = [];
+      var res = {};
+      var resList = [];
       var currentTag = tagList[tagList.length - 1];
       var filtredTagList = tagMap[currentTag] ? tagMap[currentTag] : [];
       for (var i = 0; i < filtredTagList.length; i++) {
         // console.log(filtredTagList[i])
        var flag = searchTag(filtredTagList[i], tagList);
         if(flag){
-          res.push(filtredTagList[i]); 
+          resList.push(filtredTagList[i]); 
         }
       } 
+      res[currentTag] = resList;
     }
     return res;
   }
@@ -137,15 +140,9 @@
   }
 
 
-  function myFunction() {
-    var n =  createNodesTree(document.body)
-    console.log(n);
-    console.log(classMap);
-  }
-
   function getID(){
     var val = 'ii';
-    console.log("ii node is ",getElementByID(val));
+    console.log("ii node is ",getElementById(val));
   }
   function getTag(){
     var val = 'div   span'
@@ -155,3 +152,207 @@
     var val = 'main';
     console.log(getElementsByClass(val));
   }
+
+
+function createModal(n){
+  // Get the modal
+    var modal = document.createElement('div');
+    modal.id = 'tree_modal';
+    modal.className = 'modal';
+    document.body.appendChild(modal);
+
+    // Now create and append to iDiv
+    var innerDiv = document.createElement('div');
+    innerDiv.className = 'modal-content';
+    // The variable iDiv is still good... Just append to it.
+
+
+    var sp = document.createElement('span');
+    sp.className = 'close' ;
+    sp.innerHTML = 'X';
+    sp.title = 'Close';
+    innerDiv.appendChild(sp);
+
+    var p = document.createElement('p');
+    p.className = 'col-6';
+    // recursion into a tree
+    var treehtml = document.createElement('ul');
+    createTreeHTML(n, treehtml);
+    p.appendChild(treehtml);
+    innerDiv.appendChild(p);
+
+    // aside for search tags, id, classes
+    var aside = document.createElement('aside');
+    aside.className = 'col-6 modal-aside';
+    // creating label for input
+    var label = document.createElement('label');
+    label.innerHTML = 'Search html nodes in the tree:';
+    aside.appendChild(label);
+
+    // creating a input for searching nodes
+    var inputs = document.createElement('input');
+    inputs.type = 'text';
+    inputs.name = 'search';
+    inputs.id = 'search-input';
+    inputs.placeholder = 'Enter to search';
+    inputs.addEventListener("keyup", function(e){
+      if(e.keyCode === 13){
+        searchNodeInTree(this.value);
+      }
+    });
+    aside.appendChild(inputs);
+
+    // creating help text for classes, id, and tags
+    var help = document.createElement('p');
+    help.className = 'help-block';
+    help.innerHTML = "Press Enter to search <br>Use tags to search by tags, eg: div <br> \
+                      Use '#' to search tags by id eg: #header \
+                      <br> Use: '.' to search by class, eg: .main-header <br>"
+    aside.appendChild(help);
+
+    // creatiing a result div for input search 
+    var h4 = document.createElement('h4');
+    h4.innerHTML = "Search Results: ";
+    aside.appendChild(h4);
+    var resultDiv = document.createElement('div');
+    resultDiv.id = 'resultDiv';
+    aside.appendChild(resultDiv);
+
+    innerDiv.appendChild(aside);
+
+    modal.appendChild(innerDiv);
+
+
+    // When the user clicks on <span> (x), close the modal
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+    }
+
+}
+
+var createTreeHTML = function(rootnode, treehtml) {
+    var li = document.createElement('li');
+    // span for tag node name
+    var name_span = document.createElement('span');
+    name_span.title = 'Tag';
+    name_span.innerHTML = rootnode.nodeName;
+    li.appendChild(name_span);
+    // span for id
+    var id = rootnode.id ? ' <b> #</b>' + rootnode.id : '';
+    var name_span = document.createElement('span');
+    name_span.title = 'Class';
+    name_span.innerHTML = id;
+    li.appendChild(name_span);
+    // span for class
+    var className = rootnode.classList.length ? ' <b> .</b>'+ rootnode.classList.join(', .') : '';
+    var name_span = document.createElement('span');
+    name_span.title = 'Id';
+    name_span.innerHTML = className;
+    li.appendChild(name_span);
+    
+    // append li in the html
+    treehtml.appendChild(li);
+    if(rootnode.children.length){
+        var subtree = document.createElement('ul');
+        li.appendChild(subtree);
+        for (var i = 0, length = rootnode.children.length; i < length; i++) {
+          createTreeHTML(rootnode.children[i], subtree)
+        }
+    }
+    else{
+      return true;
+    }
+
+};
+
+var searchNodeInTree = function(val){
+  if(val.charAt(0) === '.'){
+    res = getElementsByClass(val.replace(/[.]+/g,''));
+    var resultDiv = document.getElementById('resultDiv');
+    resultDiv.innerHTML = '';
+    resultDiv.className = 'results';
+    for(item in res){
+      for (var i = 0; i < res[item].length; i++) {
+        var rdiv = document.createElement('div');
+        rdiv.innerHTML = print(res[item][i]) + '<hr>';
+        resultDiv.appendChild(rdiv)
+      }
+    }
+  }
+  else if(val.charAt(0) === '#'){
+    res = getElementById(val.replace(/[#]+/g,''));
+    var resultDiv = document.getElementById('resultDiv');
+    resultDiv.className = 'results';
+    resultDiv.innerHTML = print(res);
+  }
+  else{
+    res = getElementsByTag(val);
+    var resultDiv = document.getElementById('resultDiv');
+    resultDiv.innerHTML = '';
+    resultDiv.className = 'results';
+    for(item in res){
+      for (var i = 0; i < res[item].length; i++) {
+        var rdiv = document.createElement('div');
+        rdiv.innerHTML = print(res[item][i]) + '<hr>';
+        resultDiv.appendChild(rdiv)
+      }
+    }
+  }
+  
+};
+
+var print = function(o){
+    var str='';
+    var outerD = document.createElement('div');
+    for(var p in o){
+        var innerD = document.createElement('span');
+        if(typeof o[p] == 'string'){
+           var v = o[p].length ? o[p] : null;
+            innerD.innerHTML = p + ': ' + v +'; </br>';
+        }
+        else if(p === 'parent'){
+            // innerD.innerHTML = p + ': { </br>' + print(o + '}';
+            var par = o[p] ? o[p].nodeName : null;
+            innerD.innerHTML = p + ': ' + par  + '; </br>';
+        }
+        if( p === 'children'){
+          var c = o.children.length ? o.children.length : 0;
+          innerD.innerHTML = p + ': '+ c + '; </br>';
+        }
+        if( p === 'classList'){
+          var className = o.classList.length ? o.classList.join(', ') : null; 
+          innerD.innerHTML ='class: '+ className + '; </br>';
+        }
+        outerD.appendChild(innerD);
+    }
+
+    return outerD.innerHTML;
+};
+
+
+
+  function showHTMLTREE() {
+    var n =  createNodesTree(document.body)
+    createModal(n);
+  }
+
+  function showModal(){
+    var modal = document.getElementById('tree_modal');
+    modal.style.display = 'block';
+  }
+
+  (function() {
+    showHTMLTREE();
+  })();
+
+  // window.load(function(){
+    
+  // })
